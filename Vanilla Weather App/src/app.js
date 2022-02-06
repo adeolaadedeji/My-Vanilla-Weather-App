e6d9b7619e688f39e1577c64d1f8cafe8274b422
adeolaadedeji <delaji@yahoo.com>
2022-02-06 10:46:56 +0100

    Showing the current temperature

--- Vanilla Weather App/src/app.js
+++ Vanilla Weather App/src/app.js
@@ -1,1 +1,18 @@
+function displayTemparature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "73b32b3410f09080307c21fa3b1e4394";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lagos&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemparature);
